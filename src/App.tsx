5c9d9629b0eb74df20d01361045109c481646559
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-22 14:16:11 +0200

    event hadnlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import './App.css';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Hello } from 'components/Hello';
 
 function App() {
   const [age, setAge] = useState<number>(36);
+  // React.MouseEvent
+  // React.FormEvent
+  // React.ChangeEvent
+  // const handleClick = (event: React.MouseEvent) => {
+  // React.MouseEventHandler
+  // React.FormEventHandler
+  // React.ChangeEventHandler
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    setAge((age) => age + 1);
+  }
+  const handleDivClick: React.MouseEventHandler<HTMLDivElement> = () => {
+    setAge((age) => age + 1);
+  }
   return (
     <div className="App">
-      <button onClick={() => { setAge((age) => age + 1); }}>+</button>
+      <button onClick={handleClick}>+</button>
+      <div onClick={handleDivClick}>Hello</div>
       <Hello name="Patryk" age={age} />
     </div>
   );
