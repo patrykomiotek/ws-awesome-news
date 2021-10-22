81ea993294eb8ec3455afdefc4ca1f3c65529970
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-22 14:05:55 +0200

    first handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 
+import { useState } from 'react';
 import { Hello } from 'components/Hello';
 
 function App() {
+  const [age, setAge] = useState<number>(36);
   return (
     <div className="App">
-      <Hello name="Patryk" age={36} />
+      <button onClick={() => { setAge((age) => age + 1); }}>+</button>
+      <Hello name="Patryk" age={age} />
     </div>
   );
 }
